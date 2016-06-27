1fa15dbe9800709aeb6a4fefb9100c7c8d70d320
suryateja <grandhisurya@gmail.com>
2016-06-27 18:14:45 +0530

    Added list view and detail view for music and genre

--- greedygame/music_app/urls.py
+++ greedygame/music_app/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from music_app.views import MusicTrackList, MusicTrackDetail, MusicGenreList, MusicGenreDetail
+from music_app.views import MusicTrackList, MusicTrackDetail, MusicGenreList, MusicGenreDetail, MusicTrackListForm
 
 urlpatterns = [
+    url(r'^tracks/addtrack/$', MusicTrackListForm.as_view()),
     url(r'^tracks/$', MusicTrackList.as_view()),
     url(r'^tracks/(?P<pk>[-\w]+)/$',MusicTrackDetail.as_view()),
     url(r'^genres/$',MusicGenreList.as_view()),
--- greedygame/music_app/views.py
+++ greedygame/music_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView,View
 from music_app.models import MusicTrack, MusicGenre, MusicTrackGenre
+from music_app.forms import MusicTrackForm
+from django.views.decorators.csrf import *
 
 
 class MusicTrackList(ListView):
@@ -10,16 +12,58 @@ class MusicTrackList(ListView):
     model = MusicTrack
     paginate_by = 2
 
+class MusicTrackListForm(View):
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super(MusicTrackListForm, self).dispatch(*args, **kwargs)
+    def get(self,request):
+        form = MusicTrackForm()
+        data = {'form':form}
+        return render(request, 'music_app/templates/musictrack_form.html',data)
+    def post(self,request):
+        print(request.POST,"12345678")
+        form = MusicTrackForm(request.POST)
+        # form.save()
+        if form.is_valid():
+            print("rfff")
+            x = form.cleaned_data
+            MusicsTrack = MusicTrack(x['title'],x['rating'])
+            MusicsTrack.save()
+            for genreds in x['genre'].values():
+                ids = int(genreds['genre_id'])
+                qw = MusicGenre.objects.get(genre_id=ids)
+                # print(qw)
+                print(MusicsTrack)
+                genretrack = MusicTrackGenre(track_id = MusicsTrack, genre_id= qw )
+                genretrack.save()
+
+        else:
+            form = MusicTrackForm()
+            data = {'form': form}
+            return render(request, 'music_app/templates/musictrack_form.html', data)
+
+
 
 class MusicTrackDetail(DetailView):
     template_name = 'music_app/templates/musictrack_detail.html'
     model = MusicTrack
+    # def get_object(self):
+    #     model = MusicTrack.objects.get(pk=self.kwargs['pk'])
+    #     print(model.track_id,"model")
     def get_context_data(self, **kwargs):
         context = super(MusicTrackDetail, self).get_context_data(**kwargs)
-        print(pk)
+        model = MusicTrack.objects.get(pk=self.kwargs['pk'])
+        print(model.track_id, "model")
+        music_track_genre_object = MusicTrackGenre.objects.filter(track_id = model.track_id)
+        for i in music_track_genre_object:
+            # print(i.genre_id_id,"genre_id")
+            print(MusicGenre.objects.get(genre_id = i.genre_id_id).genre_name)
+            context['Genre'] = MusicGenre.objects.filter(genre_id = i.genre_id_id)
+            print(context,"length")
+        # print(music_track_genre_object.genre_id, "genre_id")
         # context['Genre'] = MusicGenre.objects.filter(MusicTrackGenre.objects.get(MusicTrack.objects.get(track_id =self.request.GET.get('pk'))).values_list('genre_id'))
-        context['Genre'] = self.object.applied_to.filter(get_query(self.request.GET['q'],/
-        ['genre_id', 'track_id']))
+        # context['Genre'] = self.object.filter(get_query(self.request.GET['q'],['genre_id', 'track_id']))
+
         return context
 
 
